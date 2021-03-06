83feb5585bccec4a9a8d6a66ae98cca1750caac3
kubo1216 <kubokubo1216@gmail.com>
2021-03-06 06:41:06 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "名刺管理サービス"
+  end
+  
+  test "should get root" do
+    get root_path
+    assert_response :success
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "ホーム | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "ヘルプ | #{@base_title}"
   end
 
 end
