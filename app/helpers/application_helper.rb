7cf181539ff8f3ea95346bc335a7814f88609df6
kubo1216 <kubokubo1216@gmail.com>
2021-03-07 05:21:09 +0000

    静的なページの作成完了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # ページ事の完全なタイトルを返します
+  def full_title(page_title = '')
+    base_title = "名刺管理サービス"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
