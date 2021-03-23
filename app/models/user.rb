0070d9e65f5da41b8662709f3c8d039071c08f2a
kubo1216 <kubokubo1216@gmail.com>
2021-03-23 14:01:52 +0000

    Twitter認証のときは確認メールを送信しないようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,46 +6,18 @@ class User < ApplicationRecord
          :confirmable, :lockable, :timeoutable, :trackable,
          :omniauthable, omniauth_providers:[:twitter]
          
-  # def self.find_or_create_from_oauth(auth)
-  #   find_or_create_by(provider: auth["provider"], uid: auth["uid"]) do |user|
-  #     user.provider = auth["provider"]
-  #     user.uid      = auth["uid"]
-  #     user.name     = auth["info"]["nickname"]
-  #     user.email    = User.dummy_email(auth)
-  #     user.password = Devise.friendly_token[0, 20]
-  #     user.skip_confirmation!
-  #   end
-  # end
   def self.find_or_create_from_oauth(auth)
-<<<<<<< HEAD
-    find_or_create_by(provider: auth["provider"], uid: auth["uid"]) do |user|
+    user = find_or_create_by(provider: auth["provider"], uid: auth["uid"]) do |user|
       user.provider = auth["provider"]
       user.uid      = auth["uid"]
       user.name     = auth["info"]["nickname"]
       user.email    = User.dummy_email(auth)
       user.password = Devise.friendly_token[0, 20]
-=======
-    User.find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
-      user.provider = auth.provider
-      user.name   = auth.info.nickname
-      # user.avatar_url = auth.info.image
-      user.email      = User.dummy_email(auth)
-      user.password   = Devise.friendly_token[0, 20]
-      user.skip_confirmation!
->>>>>>> parent of 5d19ba4... Rspecを導入しようとしてぐちゃぐちゃになってしまった
     end
+    user.skip_confirmation!
+    user
   end
-  def self.from_omniauth(auth)
-    find_or_create_by(email: auth.info.email) do |user|
-      user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name
-      # user.remote_icon_url = auth.info.image
-      # user.profile = auth.info.description
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.skip_confirmation!
-    end
-  end  
+  
   def self.new_with_session(params, session)
     if session["devise.user_attributes"]
       new(session["devise.user_attributes"]) do |user|
